341797a1cbd2f004c11e8833636917ed891692fa
jonathan macgregor <jonathanm@bytelion.com>
2018-10-10 11:14:37 -0400

    Finish static pages

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,9 @@
 Rails.application.routes.draw do
-  get 'static_pages/home'
+  get 'application/hello'
 
   get 'static_pages/help'
 
-  root 'application#hello'
+  get 'static_pages/about'
+  get 'static_pages/home'
+  root 'static_pages#home'
 end
